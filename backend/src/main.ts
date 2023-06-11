297dba75953b7b7123e2ec028ff9e2099340e79b
Subhash-Chamara <udayanga.subash@gmail.com>
2023-06-11 17:10:21 +0530

    Server build code implemented

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,8 @@
-console.log("hello")
+import express, {json} from 'express';
+import cors from 'cors';
+
+export const app = express();
+
+app.use(cors());
+app.use(json());
+app.listen(8080,() => console.log("Server has been started at 8080"));
