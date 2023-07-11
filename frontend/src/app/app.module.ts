effa3c8e835fd8bebf29d043ced36ac90ca3c66f
Subhash-Chamara <udayanga.subash@gmail.com>
2023-07-11 18:49:58 +0530

    Styles add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskContainerComponent } from './task-container/task-container.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
